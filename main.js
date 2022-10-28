a88a7451bf28b0d4815df812e4f43b6d979f880f
hsmgebaly <hsmgebaly@gmail.com>
2022-10-28 09:12:49 +0200

    third commit

--- main.js
+++ main.js
@@ -18,6 +18,36 @@ function validation() {
     text.style.color = "#ff0000";
   }
 }
+
+// ////////////////////////////////
+// Hero Section Video popup
+const modal = document.querySelector(".hero-vid", ".close-modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
 //////////////////////////////////
 // Slider with Progress bar
 $(document).ready(function () {
